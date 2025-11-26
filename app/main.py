70b717211a14dda58a9f2fc9c6d25d22121af9aa
Kasjan Michalak <kasjan.michalak@gmail.com>
2025-11-26 21:24:23 +0000

    2nd save

--- app/main.py
+++ app/main.py
@@ -6,20 +6,16 @@ class Person:
         self.age = age
         Person.people[name] = self
 
-    def create_person_list(people: list) -> list:
+    def create_person_list(self, people: list) -> list:
         person_list = []
         for person in people:
             person_list.append(person)
 
         for person in people:
             spouse = Person.people[person["name"]]
-            if "wife" in person and person ["wife"] is not None:
+            if "wife" in person and person["wife"] is not None:
                 spouse.wife = person["wife"]
-            if "husband" in person and person ["husband"] is not None:
+            if "husband" in person and person["husband"] is not None:
                 spouse.husband = person["husband"]
 
         return person_list
-
-        for person in people:
-            if wife in person and person [wife] is not None:
-
